e9f9ad0aa4ed5f3aee7e9b85e4d09b79376a0cb4
SUHAIB SABIR <suhaibsabir110@gmail.com>
2023-10-15 02:17:39 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,101 @@
+// alert("hello SMIT");
+
+
+// alert ( "hello" )
+// var a = "Suhaib";
+// var suhaibphone =03333333333333;
+// alert(a)
+// alert(suhaibphone)
+
+// a="ali"
+// alert
+// (a)
+
+// var num=10;
+// alert(num+5);
+
+
+var first="suhaib";
+var last="sabir";
+var full = first+" "+last;
+alert(full);
+
+// mySuhaibSabir
+// CHAPTER NO:1
+    // QUESTION NO:1
+    alert("welcome");
+
+    // CHAPTER NO:2
+    // QUESTION NO:2
+
+    alert("ERROR!Please Entre The Valid Passowrd");
+
+
+    // CHAPTER NO:3
+    // QUESTION NO:3
+
+
+alert("Welcome To JS land \n Happy Coding!");
+
+
+
+    // CHAPTER NO:4
+    // QUESTION NO:4
+alert("welcome to js land");
+alert("Happy Coding\nPrevent this page from creating additional diaogs");
+alert("Hello...I can run js through my browser's console");
+
+
+
+var username="Suhaib sabir";
+alert(username)
+
+
+var message="Hello World";
+alert(message)
+
+var age=15;
+var field="certified mobile and web developer";
+alert(username);
+alert(age);
+alert(field);
+
+
+alert("pizza\n pizz \n pizz \n piz \n pi" );
+
+
+var email="my email is +suhaibsabr110@gmail.com";
+alert(email)
+
+
+// var book=a
+// CHAPTER2
+// QUESTION NO 8
+
+document.write("yah!I can write HTML content through JAVASCRIPT")
+
+var d="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+alert(d)
+
+// chapter 3
+// question no 1
+
+var age =20
+alert("my age is"+" "+age)
+
+
+// chapter 3
+// question2
+
+var q;
+var n;
+
+q= window.prompt("entre any  umber to find the sq root")
+n=number(q);
+n=math.pow(q,2);
+
+alert(n);
+
+var a;
+var c;
+ a=window.prompt("")
